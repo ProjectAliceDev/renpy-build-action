33f1e64c05bd4055a052d159d51073557e7d4c70
Marquis Kurt <software@marquiskurt.net>
2019-09-20 12:52:19 -0400

    [renpy-build] Specify launcher

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 echo "Downloading the specified Ren'Py SDK..."
 wget https://www.renpy.org/dl/$1/renpy-$1-sdk.tar.bz2
+ls | grep renpy
 tar xf renpy-$1-sdk.tar.bz2
 rm renpy-$1-sdk.tar.bz2
 mv renpy-$1-sdk ../renpy
 
 echo "Building the project..."
 rm -rf game/README.html
-../renpy/renpy.sh launcher distribute .
+../renpy/renpy.sh ../renpy/launcher distribute .

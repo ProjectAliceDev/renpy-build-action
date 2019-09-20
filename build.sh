e0a337dc2447af9246f287030811650b3819aabb
Marquis Kurt <software@marquiskurt.net>
2019-09-20 12:46:20 -0400

    [renpy-build] Fix spelling error in Ren'PY SDK name

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading the specified Ren'Py SDK..."
 wget https://www.renpy.org/dl/$1/renpy-$1-sdk.tar.bz2
-tar xf renpy-$1-sdk.tar-bz2
+tar xf renpy-$1-sdk.tar.bz2
 rm renpy-$1-sdk.tar.bz2
 mv renpy-$1-sdk ../renpy
 

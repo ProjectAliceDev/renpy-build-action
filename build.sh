ca98bf006e989f662c6a339127717eb9ce14b107
Marquis Kurt <software@marquiskurt.net>
2019-09-20 12:55:27 -0400

    [renpy-build] Try to specify the area where the SDK ZIP is

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@
 
 echo "Downloading the specified Ren'Py SDK..."
 wget https://www.renpy.org/dl/$1/renpy-$1-sdk.tar.bz2
-ls | grep renpy
-tar xf renpy-$1-sdk.tar.bz2
-rm renpy-$1-sdk.tar.bz2
-mv renpy-$1-sdk ../renpy
+tar xf ./renpy-$1-sdk.tar.bz2
+rm ./renpy-$1-sdk.tar.bz2
+mv ./renpy-$1-sdk ../renpy
 
 echo "Building the project..."
 rm -rf game/README.html

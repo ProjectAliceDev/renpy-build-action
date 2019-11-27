d4c25abe70ac2432addcb0afbadd97c17ef743a3
Proudust <proudust@gmail.com>
2019-11-27 12:49:38 +0900

    [renpy-build-actions] If build fails, make this step fail

--- build.sh
+++ build.sh
@@ -12,8 +12,10 @@ rm ./${sdk_name}.tar.bz2
 mv ./${sdk_name} ../renpy
 
 echo "Building the project at $2..."
-../renpy/renpy.sh ../renpy/launcher distribute $2
-
-built_dir=$(ls | grep '\-dists')
-echo ::set-output name=dir::$built_dir
-echo ::set-output name=version::${built_dir%'-dists'}
+if ../renpy/renpy.sh ../renpy/launcher distribute $2; then
+    built_dir=$(ls | grep '\-dists')
+    echo ::set-output name=dir::$built_dir
+    echo ::set-output name=version::${built_dir%'-dists'}
+else
+    return 1
+fi
